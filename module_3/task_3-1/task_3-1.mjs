61e6be58a96d473134340f74c89c0451e9ebd930
Daniel Bell <danieltb@uia.no>
2024-10-14 16:13:20 +0200

    3-1 to part 8

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -6,19 +6,38 @@ printOut("--- Part 1, 2, 3 -----------------------------------------------------
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
 let wakeUpTime = 2;
-if ("Replace this") {
+if (wakeUpTime <= 7) {
   printOut("I can take the bus to school.");
+} else if (wakeUpTime <=8){
+  printOut("I can take the train to school.")
+} else{
+  printOut ("I have to take the car to school.")
 }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let number = -2;
+
+if (number < 0){
+  printOut(number.toString() + " is negative")
+} else if (number >0){
+  printOut(number.toString() + " is positive")
+} else{
+  printOut(number.toString() + " is zero")
+}
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+
+
+
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
